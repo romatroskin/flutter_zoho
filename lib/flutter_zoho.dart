a80565bf43fbf0f17ff16ca3724761df5eec00aa
Alexander Gusakov <immtherion@gmail.com>
2021-06-16 02:33:37 +0300

    changed args showNativeView

--- lib/flutter_zoho.dart
+++ lib/flutter_zoho.dart
@@ -5,9 +5,19 @@ import 'package:flutter/services.dart';
 class FlutterZoho {
   static const MethodChannel _channel = const MethodChannel('flutter_zoho');
 
-  static Future<String?> showNativeView(Map<String, dynamic> args) async {
+  // static Future<String?> showNativeView(Map<String, dynamic> args) async {
+  //   try {
+  //     return _channel.invokeMethod('showNativeView', args);
+  //   } on PlatformException catch (e) {
+  //     throw 'Error ${e.message}';
+  //   }
+  // }
+
+  static Future<String?> showNativeView(
+      String ordId, String appId, String accessToken) async {
     try {
-      return _channel.invokeMethod('showNativeView', args);
+      return _channel.invokeMethod('showNativeView',
+          {'ordId': ordId, 'appId': appId, 'accessToken': accessToken});
     } on PlatformException catch (e) {
       throw 'Error ${e.message}';
     }
