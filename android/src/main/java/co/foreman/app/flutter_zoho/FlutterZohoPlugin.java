bb34c3e24b7857865ec45706b5e07708f07ef03e
Alexander Gusakov <immtherion@gmail.com>
2021-06-16 02:16:54 +0300

    android update

--- android/src/main/java/co/foreman/app/flutter_zoho/FlutterZohoPlugin.java
+++ android/src/main/java/co/foreman/app/flutter_zoho/FlutterZohoPlugin.java
@@ -6,9 +6,15 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 
-import com.zoho.deskportalsdk.DeskConfig;
-import com.zoho.deskportalsdk.ZohoDeskPortalSDK;
-import com.zoho.deskportalsdk.android.network.DeskCallback;
+//import com.zoho.deskportalsdk.DeskConfig;
+//import com.zoho.deskportalsdk.ZohoDeskPortalSDK;
+//import com.zoho.deskportalsdk.android.network.DeskCallback;
+
+import com.zoho.desk.asap.ZDPHomeConfiguration;
+import com.zoho.desk.asap.ZDPortalHome;
+import com.zoho.desk.asap.api.ZDPortalCallback;
+import com.zoho.desk.asap.api.ZDPortalException;
+import com.zoho.desk.asap.api.ZohoDeskPortalSDK;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,7 +38,9 @@ public class FlutterZohoPlugin implements FlutterPlugin, MethodCallHandler, Acti
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
-  public static ZohoDeskPortalSDK deskInstance;
+//  public static ZohoDeskPortalSDK deskInstance;
+  public static ZohoDeskPortalSDK apiProvider;
+
 
   Activity activity;
   Context context;
@@ -58,7 +66,7 @@ public class FlutterZohoPlugin implements FlutterPlugin, MethodCallHandler, Acti
 
     if (call.method.equals("setFCMId")) {
       String fcm = call.argument("fcmId");
-      deskInstance.enablePush(fcm);
+      apiProvider.enablePush(fcm);
     }
 
     if (call.method.equals("showNativeView")) {
@@ -79,33 +87,108 @@ public class FlutterZohoPlugin implements FlutterPlugin, MethodCallHandler, Acti
 
       ZohoDeskPortalSDK.Logger.enableLogs();
 
-      DeskConfig config = new DeskConfig.Builder()
+      final ZDPHomeConfiguration homeConfiguration = new ZDPHomeConfiguration.Builder()
               .showCommunity(true)
               .showCreateTicket(true)
               .showNavDrawer(true)
               .showMyTickets(true)
               .build();
-      deskInstance = ZohoDeskPortalSDK.getInstance((Application) context.getApplicationContext());
-      deskInstance.initDesk(OrgId, AppId, ZohoDeskPortalSDK.DataCenter.EU, config);
+
+      apiProvider = ZohoDeskPortalSDK.getInstance((Application) context.getApplicationContext());
+      apiProvider.initDesk(OrgId, AppId, ZohoDeskPortalSDK.DataCenter.EU);
 //                deskInstance.setThemeResource(R.style.deskTheme);
-      // if(deskInstance.isUserSignedIn()){
-      deskInstance.setUserToken(accessToken, new DeskCallback.DeskSetUserCallback() {
+       if(!apiProvider.isUserSignedIn()){
+      apiProvider.setUserToken(accessToken, new ZDPortalCallback.SetUserCallback() {
         @Override
         public void onUserSetSuccess() {
 
-          deskInstance.startDeskHomeScreen(activity);
+          ZDPortalHome.show(activity, homeConfiguration);
+//          apiProvider.startDeskHomeScreen(activity);
           result.success("true");
         }
 
         @Override
-        public void onException(DeskException e) {
-
+        public void onException(ZDPortalException e) {
           result.error("400", e.getMessage(), "false");
         }
       });
     } else {
       result.notImplemented();
     }
+  }
+
+//  Activity activity;
+//  Context context;
+//
+//  long OrgId;
+//  String AppId;
+//  String accessToken;
+////    public String ZOHO_CHANNEL = "com.zoho.deskportalsdk";
+//
+//  @Override
+//  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+//    context = flutterPluginBinding.getApplicationContext();
+//
+//    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_zoho");
+//    channel.setMethodCallHandler(this);
+//  }
+//
+//  @Override
+//  public void onMethodCall(@NonNull MethodCall call, @NonNull final Result result) {
+//    // if (call.method.equals("getPlatformVersion")) {
+//    //     result.success("Android " + android.os.Build.VERSION.RELEASE);
+//    // }
+//
+//    if (call.method.equals("setFCMId")) {
+//      String fcm = call.argument("fcmId");
+//      deskInstance.enablePush(fcm);
+//    }
+//
+//    if (call.method.equals("showNativeView")) {
+//
+//      HashMap<String, Object> map = call.arguments();
+//
+//      try {
+//        JSONObject params = new JSONObject(map);
+//        OrgId = Long.parseLong(params.getString("orgId"));
+//        AppId = params.getString("appId");
+//        accessToken = params.getString("accessToken");
+//
+//      } catch (JSONException e) {
+//        e.printStackTrace();
+//
+//        return;
+//      }
+//
+//      ZohoDeskPortalSDK.Logger.enableLogs();
+//
+//      DeskConfig config = new DeskConfig.Builder()
+//              .showCommunity(true)
+//              .showCreateTicket(true)
+//              .showNavDrawer(true)
+//              .showMyTickets(true)
+//              .build();
+//      deskInstance = ZohoDeskPortalSDK.getInstance((Application) context.getApplicationContext());
+//      deskInstance.initDesk(OrgId, AppId, ZohoDeskPortalSDK.DataCenter.EU, config);
+////                deskInstance.setThemeResource(R.style.deskTheme);
+//      // if(deskInstance.isUserSignedIn()){
+//      deskInstance.setUserToken(accessToken, new DeskCallback.DeskSetUserCallback() {
+//        @Override
+//        public void onUserSetSuccess() {
+//
+//          deskInstance.startDeskHomeScreen(activity);
+//          result.success("true");
+//        }
+//
+//        @Override
+//        public void onException(DeskException e) {
+//
+//          result.error("400", e.getMessage(), "false");
+//        }
+//      });
+//    } else {
+//      result.notImplemented();
+//    }
   }
 
   @Override
@@ -120,16 +203,16 @@ public class FlutterZohoPlugin implements FlutterPlugin, MethodCallHandler, Acti
 
   @Override
   public void onDetachedFromActivityForConfigChanges() {
-
+    activity = null;
   }
 
   @Override
   public void onReattachedToActivityForConfigChanges(@NonNull @org.jetbrains.annotations.NotNull ActivityPluginBinding binding) {
-
+    activity = binding.getActivity();
   }
 
   @Override
   public void onDetachedFromActivity() {
-
+    activity = null;
   }
 }
