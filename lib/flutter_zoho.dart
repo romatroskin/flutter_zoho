d016f088e9c104c3f045f1657ea69364471d7e9c
Alexander Gusakov <immtherion@gmail.com>
2021-06-28 16:45:48 +0300

    added setFCMId method

--- lib/flutter_zoho.dart
+++ lib/flutter_zoho.dart
@@ -5,13 +5,13 @@ import 'package:flutter/services.dart';
 class FlutterZoho {
   static const MethodChannel _channel = const MethodChannel('flutter_zoho');
 
-  // static Future<String?> showNativeView(Map<String, dynamic> args) async {
-  //   try {
-  //     return _channel.invokeMethod('showNativeView', args);
-  //   } on PlatformException catch (e) {
-  //     throw 'Error ${e.message}';
-  //   }
-  // }
+  static Future<String?> setFCMId(Map<String, dynamic> args) async {
+    try {
+      return _channel.invokeMethod('setFCMId', args);
+    } on PlatformException catch (e) {
+      throw 'Error ${e.message}';
+    }
+  }
 
   static Future<String?> showNativeView(Map<String, dynamic> args) async {
     try {
